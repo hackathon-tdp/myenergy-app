095b61fb5405bc65f70c3e48f8443c9106b72797
czolojd3000 <masllo.maslo@gmail.com>
2023-02-08 18:30:29 +0100

    changed Button, added Button types

--- src/components/base/Button.js
+++ src/components/base/Button.js
@@ -2,47 +2,69 @@ import { useContext } from "react";
 import { Pressable, StyleSheet, Text } from "react-native";
 import ThemeContext from "../ThemeContext";
 
-export const Button = ({ isFilled = true, marginTop, onPress, children }) => {
+export const Button = ({ type = 'default', marginTop, onPress, children }) => {
     const theme = useContext(ThemeContext);
 
-    const styles = isFilled
-        ? StyleSheet.create({
-              button: {
-                  marginTop: marginTop,
-                  height: 50,
-                  justifyContent: "center",
-                  alignItems: "center",
-                  backgroundColor: "#00cc99",
-                  width: "48%",
-                  borderRadius: 6,
-                  maxWidth: 160,
-              },
-              text: {
-                  color: "#fff",
-                  fontFamily: "SourceSansProSemiBold",
-                  fontSize: 16,
-              },
-          })
-        : StyleSheet.create({
-              button: {
-                  marginTop: marginTop,
-                  height: 50,
-                  justifyContent: "center",
-                  alignItems: "center",
-                  backgroundColor: "#fff",
-                  width: "48%",
-                  borderRadius: 6,
-                  maxWidth: 160,
-                  borderWidth: 3,
-                  borderColor: "#00cc99",
-                  borderStyle: "solid",
-              },
-              text: {
-                  color: "#00cc99",
-                  fontFamily: "SourceSansProSemiBold",
-                  fontSize: 16,
-              },
-          });
+    const styles =
+        type === "empty"
+            ? StyleSheet.create({
+                  button: {
+                      marginTop: marginTop,
+                      height: 50,
+                      justifyContent: "center",
+                      alignItems: "center",
+                      backgroundColor: "#fff",
+                      width: "48%",
+                      borderRadius: 6,
+                      maxWidth: 160,
+                      borderWidth: 3,
+                      borderColor: "#00cc99",
+                      borderStyle: "solid",
+                  },
+                  text: {
+                      color: "#00cc99",
+                      fontFamily: "SourceSansProSemiBold",
+                      fontSize: 16,
+                  },
+              })
+        : type === "inactive"
+            ? StyleSheet.create({
+                  button: {
+                      marginTop: marginTop,
+                      height: 50,
+                      justifyContent: "center",
+                      alignItems: "center",
+                      backgroundColor: "#cccccc",
+                      width: "48%",
+                      borderRadius: 6,
+                      maxWidth: 160,
+                      borderWidth: 3,
+                      borderColor: "#cccccc",
+                      borderStyle: "solid",
+                  },
+                  text: {
+                      color: "#999999",
+                      fontFamily: "SourceSansProSemiBold",
+                      fontSize: 16,
+                  },
+              })
+            : StyleSheet.create({
+                  button: {
+                      marginTop: marginTop,
+                      height: 50,
+                      justifyContent: "center",
+                      alignItems: "center",
+                      backgroundColor: "#00cc99",
+                      width: "48%",
+                      borderRadius: 6,
+                      maxWidth: 160,
+                  },
+                  text: {
+                      color: "#fff",
+                      fontFamily: "SourceSansProSemiBold",
+                      fontSize: 16,
+                  },
+              });
 
     return (
         <Pressable style={styles.button} onPress={onPress}>
